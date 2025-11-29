d79e3e6193649f8b31d41d996995a78b35f39669
wbnovi <~@~>
2025-11-29 14:58:06 +0100

    initial commit

---
+++ customer.java
@@ -0,0 +1,9 @@
+public class customer {
+    private String name;
+    private String supermarket;
+
+    public customer(String name, String supermarket) {
+        this.name = name;
+        this.supermarket = supermarket;
+    }
+}
---
+++ supermarket.java
@@ -0,0 +1,13 @@
+public class supermarket  {
+    private int bread;
+    private int cheese;
+    private int fruit;
+    private int toiletPaper;
+
+    public supermarket(int bread, int cheese, int fruit, int toiletPaper) {
+        this.bread = bread;
+        this.cheese = cheese;
+        this.fruit = fruit;
+        this.toiletPaper = toiletPaper;
+    }
+}
